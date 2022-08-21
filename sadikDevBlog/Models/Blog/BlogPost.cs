b554b28f24fdac124821866430926385a96db93d
sadiksa <sadiksahin95@gmail.com>
2022-08-21 15:01:07 +0300

    vue and axios were added, home page design

--- sadikDevBlog/Models/Blog/BlogPost.cs
+++ sadikDevBlog/Models/Blog/BlogPost.cs
@@ -5,6 +5,7 @@ namespace sadikDevBlog.Models.Blog
         public DateTime CreateDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public string Header { get; set; }
+        public string ImagePath { get; set; }
         public string ShortContent { get; set; }
         public BlogPostDetail Content {get; set;}
     }
