c82a300b5039ff3497a2d79608dee5708edfb4f1
sadiksa <sadiksahin95@gmail.com>
2022-08-21 17:00:32 +0300

    home page was created

--- sadikDevBlog/Controllers/HomeController.cs
+++ sadikDevBlog/Controllers/HomeController.cs
@@ -1,21 +1,28 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using sadikDevBlog.Data;
 using sadikDevBlog.Models;
+using sadikDevBlog.Models.Blog;
 
 namespace sadikDevBlog.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+    public List<BlogPost> GetListBlogPost(int startFrom, int take){
+        return _context.BlogPosts.Skip(startFrom).Take(take).ToList();
     }
 
     public IActionResult Privacy()
---
+++ sadikDevBlog/Data/Migrations/20220821121317_blogpostimagepathadded.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace sadikDevBlog.Data.Migrations
+{
+    public partial class blogpostimagepathadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "BlogPosts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "BlogPosts");
+        }
+    }
+}
